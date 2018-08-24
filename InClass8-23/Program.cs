4c01507c30f0cf83d84393c3f1ec2ce9feac420d
Thomas Lewis <thomasalewis1028@gmail.com>
2018-08-24 17:36:24 -0500

    In class assignment for 8/23

---
+++ InClass8-23/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * This file is for the in class activity on August 23rd, 2019.
+ * It outputs to console a few lines using WriteLine and closes the console on user input.
+ * I already have experiene in C#, so I'm using JetBrains Rider over Visual Studio because it's faster, prettier, more
+ * lightweight, etc etc.
+ */
+
+using System;
+
+namespace inClass823
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			//These lines output to the console
+			Console.WriteLine("Hello World\n" + 
+			                  "Thanos did nothing wrong\n" +
+			                  "Perfectly balanced, as all things should be.\n\n" +
+			                  "JetBrains Rider > Visual Studio");
+			Console.ReadLine();
+		}
+	}
+}
