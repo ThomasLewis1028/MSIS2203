7dfe96d7f4c3ee4262e5a16ff987f53b9ef31fa7
Thomas Lewis <thomasalewis1028@gmail.com>
2018-09-04 10:49:51 -0500

    Week 3 ICE 1- Initial commit

---
+++ Week3ICE1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Thomas Lewis
+ * MSIS2203
+ * September 4, 2018
+ * Week 3 In Class Excersize 1
+ */
+
+using System;
+
+namespace Week3ICE1
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Ask for the name input and receive the string input with a var
+			Console.WriteLine("What is your name?");
+			var name = Console.ReadLine();
+		
+			//Ask for the beverage input and receive the string input with a var
+			Console.WriteLine("What is your beverage of choice?");
+			var beverage = Console.ReadLine();
+			
+			//Ask for the budget input and receive it using var
+			Console.WriteLine("What is your budget?");
+			//Convert from a string to a double for math purposes
+			var budget = Convert.ToDouble(Console.ReadLine());
+			
+			//Ask for the price of the budget and receive it using var
+			Console.WriteLine("What is the price of " + beverage + "?");
+			//Convert from a string to a double for math purposes
+			var price = Convert.ToDouble(Console.ReadLine());
+			
+			//use the Math function to round down to allow decimal input but whole integer output
+			var quantity = (int)Math.Floor(budget / price);
+			
+			//Output the quantity they can get based on their budget and the price
+			if (quantity > 1)
+			{
+				Console.WriteLine(name + " can get " + quantity + " " + beverage + "s");
+			}else if (quantity < 1)
+			{
+				Console.WriteLine(name + " cannot afford " + beverage);
+			}
+			else
+			{
+				Console.WriteLine(name + " can get " + quantity + " " + beverage);
+			}
+
+			//Readline for people using the command prompt instead of an integrated console
+			Console.ReadLine();
+		}
+	}
+}
