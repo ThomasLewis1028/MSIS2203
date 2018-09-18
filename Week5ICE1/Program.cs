2a27e7f756df15e6acb56652d637f303e5b4b666
Thomas Lewis <thomasalewis1028@gmail.com>
2018-09-18 10:43:55 -0500

    Week 5 ICE 1 - Initial commit

---
+++ Week5ICE1/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Thomas Lewis
+ * MSIS2203
+ * September 18, 2018
+ * Week 5 ICE 1
+ */
+
+using System;
+
+namespace Week5ICE1
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Set up arrays
+			string[] cities = new string[5];
+			double[] tempsc = new double[5];
+			double[] tempsf = new double[5];
+			
+			//Ask for input
+			Console.WriteLine("What cities will you be visitng?");
+
+			//Iterate through arrays to receive input
+			for (int i = 0; i < 5; i++)
+			{
+				//Ask for and receive city name
+				Console.Write("City " + (i+1) + ": ");
+				cities[i] = Console.ReadLine();
+				//Ask for and receive temps
+				Console.Write("Expected temperature in " + cities[i] + ": ");
+				//Call CelcToFahr storing the C in tempsc and the converted F in tempsf
+				tempsf[i] = CelcToFahr(tempsc[i] = Convert.ToDouble(Console.ReadLine()));
+			}
+
+			//Output 
+			Console.WriteLine("The expected temperatures in Fahrenheit will be: ");
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine(cities[i] + " - " + tempsc[i] + "C, " + tempsf[i] + "F");
+			}
+
+		}
+
+		//Set up CelcToFahr receiving our double c
+		private static double CelcToFahr(double c)
+		{
+			//Return inline conversion I found on the internet
+			return (c * 1.8) + 32;
+		}
+	}
+}
---
+++ Week5ICE1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Week5ICE1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week5ICE1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B01C4D41-2CBE-43BA-A8D6-3745FB1D392C")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
