584ed911930387081c0a66240ac283718725289f
Thomas Lewis <thomasalewis1028@gmail.com>
2018-09-25 10:59:55 -0500

    Week 6 ICE 1 - Initial Commit FIXED (I do this way too often)

---
+++ Week6ICE1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Thomas Lewis
+ * MSIS2203
+ * September 25, 2018
+ * Week 6 ICE 1
+ */
+
+using System;
+
+namespace Week6ICE1
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Set up variables
+			string user, pass1 = "_";
+			string pass2 = "";
+			
+			//Ask for username even though it's unnecessary
+			Console.Out.Write("Enter your username: ");
+			user = Console.ReadLine();
+
+			//Ask for first password input
+			Console.Out.Write("Enter your password: ");
+			pass1 = Console.ReadLine();
+			
+			//Ask for second password input
+			Console.Out.Write("Enter your password again: ");
+			//Run while loop to check pass1 against pass2
+			while (!pass2.Equals(pass1))
+			{
+				//Get pass2 input
+				pass2 = Console.ReadLine();
+				
+				//Check if they match to determine output and give user feedback
+				if(!pass2.Equals(pass1))
+					Console.Write("Passwords don't match, please enter again: ");
+				else
+					Console.WriteLine("Passwords match.");
+			}
+		}
+	}
+}
